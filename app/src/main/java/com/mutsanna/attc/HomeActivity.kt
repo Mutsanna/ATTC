890856cb403cfd1b567fee6636b3ea48517d53ec
Iffatusy Syaharani <vangelic@users.noreply.github.com>
2021-06-07 13:47:53 +0700

    navbar

--- app/src/main/java/com/mutsanna/attc/HomeActivity.kt
+++ app/src/main/java/com/mutsanna/attc/HomeActivity.kt
@@ -2,10 +2,14 @@ package com.mutsanna.attc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(2).isEnabled = false
     }
 }
