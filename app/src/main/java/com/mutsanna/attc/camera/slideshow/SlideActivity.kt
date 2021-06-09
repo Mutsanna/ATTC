dda9709b7c8252f3f1106e7b4f64a980f3bf60eb
Iffatusy Syaharani <vangelic@users.noreply.github.com>
2021-06-09 22:01:43 +0700

    okey

--- app/src/main/java/com/mutsanna/attc/camera/slideshow/SlideActivity.kt
+++ app/src/main/java/com/mutsanna/attc/camera/slideshow/SlideActivity.kt
@@ -3,11 +3,13 @@ package com.mutsanna.attc.camera.slideshow
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.mutsanna.attc.R
+import com.mutsanna.attc.camera.CameraActivity
 import com.mutsanna.attc.databinding.ActivityHomeBinding
 import com.mutsanna.attc.databinding.ActivitySlideBinding
 import com.mutsanna.attc.home.HomeActivity
@@ -19,6 +21,8 @@ class SlideActivity : AppCompatActivity() {
     var onBoardingViewPagerAdapter: OnBoardingViewPagerAdapter? = null
     var dotLayout: TabLayout? = null
     var onBoardingViewPager: ViewPager? = null
+    var nextButton: ImageView? = null
+    var position = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +30,7 @@ class SlideActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         dotLayout = findViewById(R.id.layoutDots)
+        nextButton = findViewById(R.id.btn_next)
 
         val onBoardingData:MutableList<OnBoardingData> = ArrayList()
         onBoardingData.add(OnBoardingData(resources.getString(R.string.slide_salah), resources.getString(R.string.desc1), R.drawable.gbs1))
@@ -39,6 +44,42 @@ class SlideActivity : AppCompatActivity() {
             val moveIntent = Intent(this@SlideActivity, HomeActivity::class.java)
             startActivity(moveIntent)
         }
+
+        binding.btnSkip.setOnClickListener {
+            val moveIntent = Intent(this@SlideActivity, CameraActivity::class.java)
+            startActivity(moveIntent)
+        }
+
+        position = onBoardingViewPager!!.currentItem
+
+        nextButton?.setOnClickListener {
+            if (position < onBoardingData.size) {
+                position++
+                onBoardingViewPager!!.currentItem = position
+            }
+
+            if (position == onBoardingData.size) {
+                val moveIntent = Intent(this@SlideActivity, CameraActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
+
+        dotLayout!!.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                position = tab!!.position
+                if (tab.position == onBoardingData.size-1){
+                    nextButton!!.setImageResource(R.drawable.tombol_go)
+                }else{
+                    nextButton!!.setImageResource(R.drawable.tombol_next)
+                }
+            }
+        })
     }
 
     private fun setOnBoardingViewPagerAdapter(onBoardingData: List<OnBoardingData>) {
